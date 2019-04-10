355c42b31e84f61892152a6681ce7a2af60bc77b
Alison Golden <alisong779@yahoo.com>
2019-04-10 17:32:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,7 @@
 def hello_t(names)
-   new_var = names.each do |name|
-              puts "#{name}"
+  new_var = names.each do |name|
+      puts "#{name}"
     end
-  name.upcase
 end
 # call your method here!
 
